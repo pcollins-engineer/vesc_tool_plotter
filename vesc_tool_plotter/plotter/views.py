2343c38315fb7d0d070eafd6f4161883ae7471d8
Peter Collins <pcollins.engineer@gmail.com>
2020-04-26 19:55:34 -0700

    Adds log file upload and saving to server.

--- vesc_tool_plotter/plotter/views.py
+++ vesc_tool_plotter/plotter/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hi")
+    return render(request, "index.html", {})
+
+def upload(request):
+    template_data = {}
+    def handle_uploaded_file(f):
+        with open("log.csv", "wb+") as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+    handle_uploaded_file(request.FILES["log_file"])
+
+    template_data["test"] = "hi"
+
+    return render(request, "table.html", template_data)
